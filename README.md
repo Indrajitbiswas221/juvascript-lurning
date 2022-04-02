1d4b860ffc222e343b48e8f99de0874a5b0b77b8
Indrajitbiswas221 <indrajitbiswas221@gmail.com>
2022-04-02 15:10:49 +0600

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# juvascript-lurning
---
+++ modul12.js
@@ -0,0 +1,166 @@
+//how to diclar variables
+
+// var userName = 'indrajit';
+// var BananaPrise = 12;
+// var YouAreaStudent =true;
+// console.log(userName, BananaPrise, YouAreaStudent);
+
+//how to know type of a variable
+
+// var userName = 'indrajit';
+// var BananaPrise = 12;
+// var YouAreaStudent =true;
+// console.log(typeof userName);
+// console.log(typeof YouAreaStudent);
+// console.log(typeof BananaPrise);
+
+//change of variable value
+
+// var userName='Indrajit';
+//     userName='Sukanta';
+//     userName='Indrani';
+//     console.log(userName);
+
+//Discussing string
+
+// var pragraph ='My name is indrajit biswas.I am a student';
+// console.log(pragraph.toUpperCase());
+// console.log(pragraph.toLowerCase());
+// console.log(pragraph);
+// console.log(pragraph.indexOf('indrajit'));
+// console.log(pragraph.split(' '));
+
+//discustion abought number type variable
+
+// var integerNumber =25;
+// var floatNumber =23.54;
+// var stringNumber ='24.23';
+
+// console.log(integerNumber+floatNumber);
+// console.log(floatNumber+stringNumber);
+// //Change String to a number
+// stringNumber=parseFloat(stringNumber);
+// console.log(stringNumber);
+// console.log(floatNumber+stringNumber);
+// //incrase variable value
+// integerNumber=integerNumber+1;
+// console.log(integerNumber);
+// var number=34;
+// number++;
+// console.log(number);
+
+// //Change String to a number
+// var number2='44';
+// console.log(number2);
+// console.log(typeof number2);
+// number2=+number2;
+// console.log(number2);
+// console.log(typeof number2);
+
+
+//float number fruction limit
+// var number=0.1;
+// var number2=0.2;
+// var result =number+number2;
+// result=result.toFixed(5);
+// console.log(typeof result);
+
+//mathamatic equation
+// var number1=234;
+// var number2=23;
+// var total= number1+number2;
+// console.log(total);
+// var biog=number1-number2;
+// console.log(biog);
+// var gun =number1*number2;
+// console.log(gun);
+// var vag =number1/number2;
+// console.log(vag);
+// var vagsas =number1%number2;
+// console.log(vagsas);
+
+//string ar gog
+// var firstName = 'Indrajit';
+// var lastName ='Biswas';
+// console.log(firstName+' '+lastName);
+
+//convart negative number to a positive number
+// var number = -5;
+// var positivenumber =Math.abs(number);
+// console.log(positivenumber);
+
+//float to intager number
+
+// var number = 5.234;
+// var result =Math.round(number);
+// console.log(result);
+// var result2 =Math.ceil(number);
+// console.log(result2);
+// var result3 =Math.floor(number);
+// console.log(result3);
+
+//generat random number 
+// var randomNumber =Math.random();
+// console.log(randomNumber);
+
+//logic and condition
+
+//<lessthan >gaterthan condition
+// var biskutPrice=9;
+// if(biskutPrice<10){
+//     console.log('I eat Biskit');
+// }else{
+//     console.log('I eat Tee');
+// }
+
+//== Equal condition
+// var student =6;
+// if(student==5){
+//     console.log('We will go to the picnic');
+// }else{
+//     console.log('we dont go the picnic');
+// }
+
+//!= locic
+// var number =17;
+// if(number!=18){
+//     console.log('I dont wire the shirt');
+// }else{
+//     console.log('I wire the shirt');
+// }
+
+//&& condition
+// var job=true;
+// var savings = 20000;
+// if(job==true && savings==200000){
+//     console.log('Tomar biya valo mayar sanga haba');
+// }else{
+//     console.log('tomar biya valo mayarsanga habana');
+// }
+
+// || condition
+
+// var job=true;
+//  var savings = 1200;
+//  if(job==true || savings>=1200000){
+//     console.log('Tomar biya valo mayar sanga haba');
+// }else{
+//     console.log('tomar biya valo mayarsanga habana');
+// }
+
+// elseif condition
+// var job=false;
+//  var savings = 200000;
+//  if(job==true&&savings>=200000){
+//     console.log('Tomar biya valo mayar sanga haba');
+// }else if(savings>=2000000 || job==true){
+//     console.log('tomar biya mota muti mayarsanga haba');
+// }else{
+//     console.log('tomar biya mota muti mayarsanga habana');
+// }
+var date =new Date;
+console.log(date);
+
+var date2 =new Date(1971-03-23);
+console.log(date2);
+
