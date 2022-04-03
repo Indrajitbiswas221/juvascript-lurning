eb415f7ca73c7846afd67d903eac58898290dc1f
Indrajitbiswas221 <indrajitbiswas221@gmail.com>
2022-04-03 15:29:42 +0600

    first commit

---
+++ module13.js
@@ -0,0 +1,37 @@
+//array 
+// var frandAge = [15, 17, 14, 16];
+// console.log(frandAge);
+// var sonaliAge=frandAge[1];
+// console.log(sonaliAge);
+// //change array value
+// frandAge[1]=24;
+// console.log(frandAge);
+// var position = frandAge.indexOf(14);
+// console.log(position);
+//video 3
+//push pop
+// var frendAge =[13, 15, 17, 18,19];
+// console.log(frendAge.length);
+// frendAge.push(20);
+// console.log(frendAge);
+// console.log(frendAge.length);
+// frendAge.pop();
+// console.log(frendAge);
+
+// var frendAge =['Indrajit', 'sukanta', 'srikanta', 'prosan', 'Hriday', 'Anupam', 'beraj'];
+// console.log(frendAge);
+// frendAge.shift();
+// console.log(frendAge);
+// frendAge.unshift('Indro');
+// console.log(frendAge);
+// var part =frendAge.slice(2);
+// console.log(part);
+// var parts =frendAge.slice(2, 5);
+// console.log(parts);
+// let myFish = ['angel', 'clown', 'drum', 'mandarin', 'sturgeon']
+// let removed = myFish.splice(3, 2, 'what');
+// console.log(removed);
+// console.log(myFish);
+
+//video5
+
