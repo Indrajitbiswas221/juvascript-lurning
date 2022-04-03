16a07db5b1505042735ab801c91e90e890dc1351
Indrajitbiswas221 <indrajitbiswas221@gmail.com>
2022-04-03 19:20:21 +0600

    what

--- module13.js
+++ module13.js
@@ -34,4 +34,87 @@
 // console.log(myFish);
 
 //video5
+//while loop
+// var number=0;
+// while(number<15){
+//     console.log(number);
+//     number++;
+// }
+// var number=0;
+// while(number<=15){
+//     console.log(number);
+//     number++;
+// }
 
+//for loop
+// for(var i=0; i<=10; i++){
+//     console.log(i);
+// }
+
+// var number=[15, 20, 30, 55,66];
+// for(i=0; i<number.length; i++){
+//     var element=number[i];
+//     console.log(element);
+// }
+
+//video 7
+//switch 
+
+// var number =5;
+// switch (number){
+//     case 1000:
+//         console.log('I am 1000');
+//         break;
+//     case 100:
+//         console.log('I am Thousend');
+//         break;
+//     case 50:
+//         console.log('I am fifty');
+//         break;
+//     case 10:
+//     case 5:
+//         console.log('I am either 10 or 5' );
+//         break;
+//     default:
+//         console.log('I dont now who are you');
+
+// }
+
+//function
+// function sayLoveYou(){
+//     console.log('jannu baby tumi koi');
+//     console.log('mou baby tumi koi');
+// }
+// sayLoveYou();
+
+// function doubleLet(number){
+//  var result=number*2;
+//  console.log(result);
+// }
+// doubleLet(23);
+// function doubleLet(number){
+//     var result=number*2;
+//     return result;
+//    }
+//    var result=doubleLet(23);
+//    var result2=result+50;
+//    var total=result+result2;
+//    console.log(total);
+
+//ouvject
+
+// var student ={id:121, phone:1234567, name:'Indrandrajit'};
+// var student2={id:212, phone:2345566, name:'sukanta'};
+// console.log(student);
+// console.log(student2);
+// var phoneNo=student.phone;
+// var phoneNo2=student2['phone'];
+// console.log(phoneNo);
+// console.log(phoneNo2);
+// var updatephone=student.phone=01755335;
+// var phoneNo=student.phone;
+// console.log(updatephone);
+// var updatephone2=student2['phone']=90988746;
+// console.log(updatephone2);
+// student.chenama='ogni2';
+// console.log(student);
